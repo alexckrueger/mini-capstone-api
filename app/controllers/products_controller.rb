f05901bdc02d179db7eccb0ba171072ab2f81876
Alex Krueger <alexckrueger@gmail.com>
2022-01-12 12:00:20 -0600

    adds individual saleable black market items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,24 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def liver
+    products = Product.find_by(name: "liver")
+    render json: products.as_json
+  end
+
+  def heart
+    products = Product.find_by(name: "heart")
+    render json: products.as_json
+  end
+
+  def kidney
+    products = Product.find_by(name: "kidney")
+    render json: products.as_json
+  end
+
+  def brain
+    products = Product.find_by(name: "brain")
+    render json: products.as_json
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   get "/black_market" => "products#black_market_method"
 
+  get "/black_market_liver" => "products#liver"
+
+  get "/black_market_heart" => "products#heart"
+
+  get "/black_market_kidney" => "products#kidney"
+
+  get "/black_market_brain" => "products#brain"
+
 end
