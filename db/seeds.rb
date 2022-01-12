c3ced704c360394a48e1937467dbbb96be91fd00
Alex Krueger <alexckrueger@gmail.com>
2022-01-12 11:53:51 -0600

    adds /black_market

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Product.create(name: "liver", price: 80, description: "Poor uncle Rick.")
-Product.create(name: "heart", price: 150, description: "It was working when I took it out")
-Product.create(name: "kidney", price: 55, description: "Can not guarantee working condition")
-Product.create(name: "brain", price: 6000, description: "When you really need to delete a memory")
+# Product.create(name: "liver", price: 80, description: "Poor uncle Rick.")
+# Product.create(name: "heart", price: 150, description: "It was working when I took it out")
+# Product.create(name: "kidney", price: 55, description: "Can not guarantee working condition")
+# Product.create(name: "brain", price: 6000, description: "When you really need to delete a memory")
